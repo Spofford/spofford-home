74e8910075a0e293ee4a5d6e1b0df495ea39de93
amhasler <amhasler@gmail.com>
2018-07-26 12:40:58 -0400

    add concepts as children

--- app/components/Feedback/index.js
+++ app/components/Feedback/index.js
@@ -2,11 +2,21 @@ import React from "react"
 import * as contentful from 'contentful'
 const ReactMarkdown = require('react-markdown')
 import style from "./style.css"
+import { Link } from 'react-router-dom'
 
 export class Feedback extends React.Component {
-  state = {
-    model: {}
+
+  // Added this:
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      model: {},
+      children: [],
+      concepts: []
+    }
   }
+
 
   client = contentful.createClient({
     space: 'cahjy08ew1qz',
@@ -14,10 +24,35 @@ export class Feedback extends React.Component {
   })
 
   componentDidMount() {
+    let query = this.props.match.params.object
     this.fetchModel().then(this.setModel);
+    if (query==='start') {
+      this.fetchChildren().then(this.setChildren)
+    } else if (query!='start'&&query!='finish') {
+      this.fetchConcepts().then(this.setConcepts)
+    }
   }
 
-  fetchModel = () => this.client.getEntry(this.props.match.params.entity)
+  fetchModel() {
+    if (this.props.match.params.object==='start') {
+      return this.client.getEntry('3XtovxD6w0AQ6U4ieSiIow')
+    } else if (this.props.match.params.object==='finish') {
+      return this.client.getEntry('6ofWITems0U0OqGM6W42GU')
+    } else {
+      return this.client.getEntry(this.props.match.params.object)
+    }
+  }
+
+  fetchChildren() {
+    return this.client.getEntries({ content_type: 'object' })
+  }
+
+  fetchConcepts() {
+    return this.client.getEntries({
+      content_type: 'concept',
+      'fields.object.sys.id': this.props.match.params.object
+    })
+  }
 
   setModel = response => {
     this.setState({
@@ -25,13 +60,56 @@ export class Feedback extends React.Component {
     })
   }
 
+  setChildren = response => {
+    this.setState({
+      children: response.items
+    })
+  }
+
+  setConcepts = response => {
+    this.setState({
+      concepts: response.items
+    })
+  }
+
   render() {
-
-    return (
-      <div className="feedback">
-
+    if (this.props.match.params.object==='start') {
+      return (
+      <div className="feedbackBody">
+        <h2>{this.state.model.head}</h2>
+        <h3>{this.state.model.subhead}</h3>
+        <ReactMarkdown source={this.state.model.bodyText} />
+        <ul>
+          {this.state.children.map(item =>
+            <li key={item.sys.id}><Link to={'/feedback/' + item.sys.id}>{item.fields.head}</Link></li>
+          )}
+        </ul>
       </div>
-    )
+      )
+    } else if (this.props.match.params.object==='finish') {
+      return (
+        <div className="feedbackBody">
+          <h2>{this.state.model.head}</h2>
+          <h3>{this.state.model.subhead}</h3>
+          <ReactMarkdown source={this.state.model.bodyText} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="feedbackBody">
+          <h2>{this.state.model.head}</h2>
+          <ReactMarkdown source={this.state.model.background} />
+          <h2>{this.state.model.problemInsights}</h2>
+          <ReactMarkdown source={this.state.model.problem} />
+          {this.state.concepts.map(item =>
+            <div key={item.sys.id} className="conceptContainer">
+              <h3>{item.fields.conceptName}</h3>
+              <img src={item.fields.primaryImage.fields.file.url} />
+            </div>
+          )}
+        </div>
+      )
+    }
   }
 }
 
--- app/index.js
+++ app/index.js
@@ -5,12 +5,12 @@ import { Switch, HashRouter, Route, Redirect } from 'react-router-dom'
 
 import { default as About } from "./components/About"
 import { default as Home } from "./components/Home"
-import { default as List } from "./components/List"
 import { default as Settings } from "./components/Settings"
 import { default as Studios } from "./components/Studios"
 import { default as Header } from "./components/Header"
 import { default as Footer } from "./components/Footer"
 import { default as Post } from "./components/Post"
+import { default as Feedback } from "./components/Feedback"
 
 const App = props => (<div>{props.children}</div>)
 
@@ -22,11 +22,10 @@ ReactDOM.render(
         <div className="page-wrap">
           <Route path="/about" component={About} />
           <Route exact path='/' component={Home} />
-          <Route path="/list" component={List} />
           <Route path="/settings" component={Settings} />
           <Route path="/studios" component={Studios} />
           <Route path="/post/:entity" component={Post} />
-          <Route path="/feedback/:entity" component={Post} />
+          <Route path="/feedback/:object" component={Feedback} />
         </div>
         <Footer />
       </App>
